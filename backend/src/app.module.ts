84414ec567dab09493823082e57aa1ce6ef099cc
DIMMITI KUSHVANTH VENKATA PHANINDRA <238w1a1286@vrsec.ac.in>
2026-04-23 22:01:42 +0530

    changed the backend url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { StudentsModule } from './students/students.module';
@@ -19,5 +20,6 @@ import { CertificationTypesModule } from './certification-types/certification-ty
     DocumentsModule,
     CertificationsModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
